aff31f33990ab2f91c07243b2b58bc96a8d01952
Olimpiya Obreshkova <ollydimitrova94@gmail.com>
2023-11-20 11:50:22 +0100

    update libft.h

--- libft.h
+++ libft.h
@@ -47,14 +47,13 @@ void	*ft_memchr(const void *s, int c, size_t n)
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 char	*ft_strdup(const char *s)
 
-ft_calloc
-ft_atoi();
-
+void	*ft_calloc(size_t count, size_t size)
+int	ft_atoi(const char *str)
 
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
-char	**ft_split(char const *s1, char const *set);
+char	**ft_split(const char *s, char c)
 char	**ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char ));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
